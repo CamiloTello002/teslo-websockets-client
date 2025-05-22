bde16fdeeae82a20da4d2bd86506b494822bc3ef
CamiloTello002 <camilojtello5@gmail.com>
2025-05-22 10:45:05 -0500

    feat: add listener for connection status

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,8 +1,31 @@
-import { Manager } from 'socket.io-client'
+import { Manager, Socket } from 'socket.io-client'
+
+enum ConnectStatus {
+  connect = 'connect',
+  disconnect = 'disconnect',
+}
+
 
 export function connectToServer() {
   const manager = new Manager('http://localhost:3000/socket.io/socket.io.js')
 
   const socket = manager.socket('/')
+
   console.log({ socket })
+
+  addListeners(socket);
+}
+
+
+export function addListeners(socket: Socket) {
+
+  const serverStatusLabel = document.getElementById('server-status')!;
+
+  socket.on(ConnectStatus.connect, () => {
+    serverStatusLabel.innerHTML = 'connected'
+  })
+
+  socket.on(ConnectStatus.disconnect, () => {
+    serverStatusLabel.innerHTML = 'disconnected'
+  })
 }
